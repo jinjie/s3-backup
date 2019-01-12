cdeca86672517547dcdd521ff65507a90d27d730
Kong Jin Jie <jinjie@swiftdev.sg>
2019-01-12 16:02:26 +0800

    First Import

---
+++ README.md
@@ -0,0 +1,32 @@
+# S3 Backup Script
+
+Script I used on servers I manage to backup data files and mysql dump to S3. 
+
+No fuss, just works.
+
+## Requirements:
+
+[AWS Command Line Interface](https://aws.amazon.com/cli/)
+
+`pip install awscli`
+
+## How to use
+
+1. Create a new s3 or use an existing one.
+2. Create a new user with access key 
+3. Run `aws configure` to configure access key. This will create a file `credentials` in ~/.aws/
+4. Configure cron job to run this script based on your backup schedule.
+
+## Folder structure
+
+Backup files will be saved in the bucket with the following structure.
+
+```
+<s3-bucket-name>/
+  +-- <server-hostname>/
+  |   +-- data/   
+  |   +-- mysql/
+  +-- <server-hostname>/
+      +-- data/   
+      +-- mysql/
+```
---
+++ s3-backup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+source config
+
+DATA_UPLOAD_FILES=()
+MYSQL_UPLOAD_FILES=()
+
+# Backup files and databases
+for dir in "${BACKUP_DIRS[@]}"
+do
+    BACKUP_FILE=$TMP_DIR/${dir//[^a-zA-Z0-9]/_}-`date +%Y%m%d-%H%M%S`.tar.gz
+    tar czvPf $BACKUP_FILE $dir
+    DATA_UPLOAD_FILES+=($BACKUP_FILE)
+done
+
+if [ "${#BACKUP_DB[@]}" = 0 ]
+then
+    BACKUP_FILE=$TMP_DIR/all-`date +%Y%m%d-%H%M%S`.sql.gz
+
+    if [ -z "$MYSQL_PASS" ]
+    then
+        mysqldump -u $MYSQL_USER $db --all-databases | gzip > $BACKUP_FILE
+    else
+        mysqldump -u $MYSQL_USER -p $MYSQL_PASS $db --all-databases | gzip > $BACKUP_FILE
+    fi
+
+    echo "Dumped all databases"
+
+    MYSQL_UPLOAD_FILES+=($BACKUP_FILE)
+
+else
+    for db in "${BACKUP_DB[@]}"
+    do
+        BACKUP_FILE=$TMP_DIR/${db}-`date +%Y%m%d-%H%M%S`.sql.gz
+
+        if [ -z "$MYSQL_PASS" ]
+        then
+            mysqldump -u $MYSQL_USER $db | gzip > $BACKUP_FILE
+        else
+            mysqldump -u $MYSQL_USER -p $MYSQL_PASS $db | gzip > $BACKUP_FILE
+        fi
+
+        echo Dumped database: $db
+
+        MYSQL_UPLOAD_FILES+=($BACKUP_FILE)
+    done
+fi
+
+
+## Upload files to S3
+for file in "${DATA_UPLOAD_FILES[@]}"
+do
+    $AWS_BIN s3 cp $file s3://$BUCKET/$HOSTNAME/data/
+    echo Uploaded $file...
+
+    rm -f $file
+    echo Removed $file...
+done
+
+for file in "${MYSQL_UPLOAD_FILES[@]}"
+do
+    $AWS_BIN s3 cp $file s3://$BUCKET/$HOSTNAME/mysql/
+    echo Uploaded $file...
+
+    rm -f $file
+    echo Removed $file...
+done
